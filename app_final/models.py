8f896eb11b1919ee13c948a7cc067ef409ef544e
Matias Pretti <matias.pretti@gmail.com>
2022-08-15 14:56:33 -0300

    creacion de models y DB

--- app_final/models.py
+++ app_final/models.py
@@ -1,3 +1,21 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class Tecnicos(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    turno = models.CharField(max_length=40)
+    fecha_ingreso = models.DateField()
+
+class Sucuarsales(models.Model):
+
+    zona = models.CharField(max_length=40)
+    rsoc = models.CharField(max_length=40)
+
+class Vehiculo(models.Model):
+
+    marca = models.CharField(max_length=40)
+    modelo = models.CharField(max_length=40)
+    
